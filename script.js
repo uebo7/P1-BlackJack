ca9b96c510be2d229ab3129eed6791f1134b3ea9
Logan <logkauf14@gmail.com>
2022-12-14 20:28:02 -0500

    'completes AJAX and jQuery additions'

--- script.js
+++ script.js
@@ -1,16 +1,22 @@
-$.ajax('https://type.fit/api/quotes').then(function(data) {console.log(data)})
+
 
 $(document).ready(function() {
     
     $("button").on("click", function() {
-        let $num = 1 + Math.floor(Math.random() * 221347)
-        const $div = $("div")
-        $div.empty()
-        $div.append($num)
+        const result = $.ajax('https://type.fit/api/quotes').then(function(data) {
+            
+            let $num = 1 + Math.floor(Math.random() * 1643)
+            console.log(JSON.parse(data).length)
+            console.log(JSON.parse(data)[$num].text)
+            const $div = $("div")
+            $div.empty()
+            $div.append((JSON.parse(data)[$num].text))
+})
+        
     })
 
 
 
 })
 
-// 221347 = amount of quotes
+// 1643 = amount of quotes

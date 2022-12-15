4834757ee5718caab2a54300adf762300b8c79de
Logan <logkauf14@gmail.com>
2022-12-14 21:10:00 -0500

    stages changes for development

--- script.js
+++ script.js
@@ -4,19 +4,11 @@ $(document).ready(function() {
     
     $("button").on("click", function() {
         const result = $.ajax('https://type.fit/api/quotes').then(function(data) {
-            
-            let $num = 1 + Math.floor(Math.random() * 1643)
-            console.log(JSON.parse(data).length)
-            console.log(JSON.parse(data)[$num].text)
-            const $div = $("div")
-            $div.empty()
-            $div.append((JSON.parse(data)[$num].text))
+        let $num = 1 + Math.floor(Math.random() * 1643);
+        const $div = $("div");
+        $div.empty();
+        $div.append((JSON.parse(data)[$num].text));
 })
-        
     })
-
-
-
 })
 
-// 1643 = amount of quotes

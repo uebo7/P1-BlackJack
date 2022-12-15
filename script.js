4e92a96e4ed1e5f1d2ab63d9f37a25ca4c2cf2ad
Logan <logkauf14@gmail.com>
2022-12-14 19:29:55 -0500

    'adds jQuery for random number'

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 $.ajax('https://type.fit/api/quotes').then(function(data) {console.log(data)})
+
+$(document).ready(function() {
+    
+    $("button").on("click", function() {
+        let $num = 1 + Math.floor(Math.random() * 221347)
+        const $div = $("div")
+        $div.empty()
+        $div.append($num)
+    })
+
+
+
+})
+
+// 221347 = amount of quotes

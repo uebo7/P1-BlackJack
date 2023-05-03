ff30062e06247ee2454ff6800260573e6cc7e69c
Logan <logkauf14@gmail.com>
2023-05-03 13:14:07 -0400

    fix: adds id to div

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ $(document).ready(function() {
     $("button").on("click", function() {
         const result = $.ajax('https://type.fit/api/quotes').then(function(data) {
         let $num = 1 + Math.floor(Math.random() * 1643);
-        const $div = $("div");
+        const $div = $("#quote");
         $div.empty();
         $div.append((JSON.parse(data)[$num].text));
 })
